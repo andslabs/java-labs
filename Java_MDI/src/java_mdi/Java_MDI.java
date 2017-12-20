60032a79443b895101ed2fedd9c2fd0d95ef051b
Andrew <32079115+andslabs@users.noreply.github.com>
2017-12-21 00:19:14 +0200

    Add files via upload

---
+++ Java_MDI/src/java_mdi/Java_MDI.java
@@ -0,0 +1,15 @@
+package java_mdi;
+
+/**
+ * @author return0
+ */
+public class Java_MDI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
